6bf0f91e6537c20e3cf9833411b53765678ce9de
Noam Peleg <49092985+noampl@users.noreply.github.com>
2022-04-06 19:48:02 +0300

    Update ContactViewModel.java

--- app/src/main/java/com/example/jesta/viewmodel/ContactViewModel.java
+++ app/src/main/java/com/example/jesta/viewmodel/ContactViewModel.java
@@ -1,10 +1,10 @@
 package com.example.jesta.viewmodel;
 
 import com.example.jesta.common.Consts;
-
+import androidx.lifecycle.ViewModel;
 import java.util.regex.Pattern;
 
-public class ContactViewModel {
+public class ContactViewModel extends ViewModel {
 
     public void sendMessage(String firstName, String lastName, String email, String subject, String message) {
 
